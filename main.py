b336f7fddb7b323ee3b866e57529003d1946fa4b
prepodobnuy <zvfvbycsy6@gmail.com>
2023-09-05 19:50:54 +0300

    new version!

--- main.py
+++ main.py
@@ -1,136 +1,91 @@
-class main():
-	def __init__(self):
-		self.iter = 0
-
-		self.cap = 10
-
-		list = []
-
-		file = open('settings.conf')
-		while True:
-			line = file.readline()
-
-			if not line:
-				break
-
-			list.append(line)
-		file.close()
-
-		self.path = list[0][6::][::-1][1::][::-1] + '/'
+def main():
+	
+	with open('settings.conf') as file:
 		
-		self.extension = list[1][11::]
-
-		self.messageHandler()
-
-	def merge(self) -> str:
-		return self.path + str(self.iter) + self.extension
-
-	def rewriteSettings(self):
-		towrite = f"path= {self.path}\nextention= {self.extension}"
-
-		file = open('settings.conf', 'w+')
-		file.write(towrite)
-		file.close()
-
-	def messageHandler(self):
-		print('help for command list')
+		config = file.read().split('\n')
+		conf = []
+		for half in config:
+			conf.append(half.split('=')[1])
+    
+	def confedit():
 
 		run = True
 
+		def write(towrite):
+			with open('settings.conf','w+') as file:
+				file.write(f'''path={towrite[0]}
+extention={towrite[1]}
+filecount={towrite[2]}
+stringlen={towrite[3]}
+stringsinfile={towrite[4]}''')
+
+		def path(path):
+			conf[0] = path
+			write(conf)
+		
+		def exte(exte):
+			conf[1] = exte
+			write(conf)
+
+		def filecount(num):
+			conf[2] = num
+			write(conf)
+
+		def strlen(num):
+			conf[3] = num
+			write(conf)
+
+		def strinfile(num):
+			conf[4] = num
+			write(conf)
+
 		while run:
+			task = input('>>')
+			if task == 'help': 
+				print('''Config commands:
+set path [path] - - - - - sets spam folder
+set extension [ext] - - - sets spam files extension
+set filecount [num] - - - sets spam files number
+set stringlen [num] - - - sets length of 1 string in file
+set stringsinfile [num] - sets num of strings in file
 
-			message = input('>>')
-
-			if message.strip() == 'exit':
+back - return to previous mode
+''')		
+			elif 'set' in task:
+				if 'path' in task:
+					path(task.split(' ')[2])
+				elif 'extension' in task:
+					exte(task.split(' ')[2])
+				elif 'filecount' in task:
+					filecount(int(task.split(' ')[2]))
+				elif 'stringlen' in task:
+					strlen(int(task.split(' ')[2]))
+				elif 'stringsinfile' in task:
+					strinfile(int(task.split(' ')[2]))
+			
+			elif 'back' in task:
 				run = False
 	
-			elif 'change' in message:
-				if len(message.strip()) == 6:
-					print(self.path + self.extension)
-	
-				else:
-					try:
-						list = message.split()
+	def start():
+		for i in range(int(conf[2])):
+			with open(f'{conf[0]}{i}{conf[1]}', 'w+') as file:
+				for j in range(int(conf[3])):
+					file.write((f'{j}'*int(conf[4])) + '\n')
 
-						if len(list) == 3:
-							if list[1].strip() == 'dir':
-								if list[2].strip()[len(list[2].strip())-1] == '/':
-									self.path = list[2].strip()
+	while True:
+		task = input('>>')
+		if task == 'help':
+			print('''Spam commands:
+conf - enter config edit mode
+start - start spam
+exit - exit from program
+''')
+		elif task == 'conf':
+			confedit()
+		elif task == 'start':
+			start()
+		elif task == 'exit':
+			exit()
 
-									self.rewriteSettings()
-
-								else:
-									self.path = list[2].strip() + '/'
-
-									self.rewriteSettings()
-
-							elif list[1].strip() == 'ext':
-								if list[2].strip()[0] == '.':
-									self.extension = list[2].strip()
-
-									self.rewriteSettings()
-
-								else:
-									self.extension = '.' + list[2].strip() 
-
-									self.rewriteSettings()
-
-					except BaseException:
-						print('Error in change!')
-
-			elif 'set' in message:
-				try:
-					list = message.split()
-					if len(list) > 1:
-						if list[1] == 'inf':
-							self.cap = list[1]
-						else:
-							self.cap = int(list[1])
-					else: 
-						print(self.cap)
-				except BaseException:
-					print('Error in set!')
-
-			elif message.strip() == 'start':
-				while True:
-					if self.cap == self.iter:
-						break
-
-					file = open(self.merge(), 'w+')
-					for i in range(10000):
-						file.write(str(i)*1000)
-
-					file.close()
-					self.iter += 1
-				self.iter = 0
-				print('>>>>Spam finished')
-
-			elif message.strip() == 'help':
-				print("""
-				Command list:
-----------------------------------------------------------------------------
-|change dir|ext [dir|extension] -> changes directory|file extension to spam|
-|#change dir /home/neformal/Документы/test                                 |
-|#change ext .txt                                                          |
-----------------------------------------------------------------------------
-|set [number of files] -> sets number of spam files                        |
-|#set inf //infinit files                                                  |
-|#set 5 //5 files                                                          |
-----------------------------------------------------------------------------
-|start -> starts spam                                                      |
-----------------------------------------------------------------------------
-|exit -> shutdown programm                                                 |
-----------------------------------------------------------------------------
-|help -> print this message                                                |
-----------------------------------------------------------------------------
-|about -> print message about this programm                                |
-----------------------------------------------------------------------------
-					""")
-			elif message.strip() == 'about':
-				print ("""
-Welcome to dirshitter!
-This program filling directory with text files.
-My github: https://github.com/Prepodobnuy
-					""")
 if __name__ == '__main__':
-	app = main()
+	main()
